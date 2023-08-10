02a9e97f5fd0ffa0e7d264bfb6366a6d0ca83faf
Umut Ozgur Delimehmetoglu <ozgdmo@gmail.com>
2023-08-10 23:32:36 +0300

    STM32F407 projesi oluşturuldu. Gerekli board spesifik CMSIS kütüphaneleri eklendi.

---
+++ dmoRTOS/Debug/Src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.Rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c 
+
+OBJS += \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o 
+
+C_DEPS += \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"C:/workspace/sau_ce_tasarim_calismasi/dmoRTOS/dmoRTOS/../chip_headers/Device" -I"C:/workspace/sau_ce_tasarim_calismasi/dmoRTOS/dmoRTOS/../chip_headers/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+
+.PHONY: clean-Src
+
